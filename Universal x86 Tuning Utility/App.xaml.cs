8ae5bb42c09f65c5da3f1ce828e4e0f698531bc9
James Craddock-Jones <jamescraddockjones@icloud.com>
2023-06-29 18:46:22 +0100

    Version bump to 1.0.9

--- Universal x86 Tuning Utility/App.xaml.cs
+++ Universal x86 Tuning Utility/App.xaml.cs
@@ -96,7 +96,7 @@ namespace Universal_x86_Tuning_Utility
             return principal.IsInRole(WindowsBuiltInRole.Administrator);
         }
 
-        public static string version = "1.0.8";
+        public static string version = "1.0.9";
         private Mutex mutex;
         private const string MutexName = "UniversalX86TuningUtility";
         /// <summary>
