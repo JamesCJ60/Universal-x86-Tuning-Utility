fab46bc47709016c0fe2d26aedff2243c89a3c05
JamesCJ60 <jamescraddockjones@icloud.com>
2023-10-22 12:38:28 +0100

    Version 2.1.3 bump

--- Universal x86 Tuning Utility/App.xaml.cs
+++ Universal x86 Tuning Utility/App.xaml.cs
@@ -61,7 +61,7 @@ namespace Universal_x86_Tuning_Utility
             return principal.IsInRole(WindowsBuiltInRole.Administrator);
         }
 
-        public static string version = "2.1.2";
+        public static string version = "2.1.3";
         private Mutex mutex;
         private const string MutexName = "UniversalX86TuningUtility";
 
