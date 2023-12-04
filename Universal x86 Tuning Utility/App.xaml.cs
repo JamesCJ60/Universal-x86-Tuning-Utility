f77ed824f31fff3600156959f8abfc91d34c9ea0
James Craddock-Jones <jamescraddockjones@icloud.com>
2023-12-04 22:34:08 +0000

    Bug fixes

--- Universal x86 Tuning Utility/App.xaml.cs
+++ Universal x86 Tuning Utility/App.xaml.cs
@@ -61,7 +61,7 @@ namespace Universal_x86_Tuning_Utility
             return principal.IsInRole(WindowsBuiltInRole.Administrator);
         }
 
-        public static string version = "2.2.1";
+        public static string version = "2.2.2";
         private Mutex mutex;
         private const string MutexName = "UniversalX86TuningUtility";
 
