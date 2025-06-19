1b236f2c3880a3609c6ad2f81c75cf6024ea9918
Misaka-12450 <8287084+Misaka-12450@users.noreply.github.com>
2025-06-19 23:01:59 +1000

    Fix #303

--- Universal x86 Tuning Utility/App.xaml.cs
+++ Universal x86 Tuning Utility/App.xaml.cs
@@ -1,32 +1,34 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System.Diagnostics;
-using System;
-using System.IO;
-using System.Reflection;
-using System.Windows;
-using System.Windows.Threading;
-using Universal_x86_Tuning_Utility.Models;
-using Universal_x86_Tuning_Utility.Services;
-using Wpf.Ui.Mvvm.Contracts;
-using Wpf.Ui.Mvvm.Services;
-using System.Security.Principal;
-using System.Windows.Input;
-using Universal_x86_Tuning_Utility.Properties;
-using System.Configuration;
-using System.Net.NetworkInformation;
-using Universal_x86_Tuning_Utility.Scripts.Misc;
-using Universal_x86_Tuning_Utility.Scripts;
-using System.Threading.Tasks;
-using System.Threading;
 using Microsoft.Extensions.Logging;
+using Microsoft.Win32;
 using RyzenSmu;
 using Serilog;
 using Serilog.Sinks.SystemConsole.Themes;
-using Universal_x86_Tuning_Utility.Scripts.ASUS;
-using Universal_x86_Tuning_Utility.Views.Pages;
+using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Reflection;
+using System.Security.Policy;
+using System.Security.Principal;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+using Universal_x86_Tuning_Utility.Models;
+using Universal_x86_Tuning_Utility.Properties;
+using Universal_x86_Tuning_Utility.Scripts;
 using Universal_x86_Tuning_Utility.Scripts.AMD_Backend;
+using Universal_x86_Tuning_Utility.Scripts.ASUS;
+using Universal_x86_Tuning_Utility.Scripts.Misc;
+using Universal_x86_Tuning_Utility.Services;
+using Universal_x86_Tuning_Utility.Views.Pages;
+using Wpf.Ui.Mvvm.Contracts;
+using Wpf.Ui.Mvvm.Services;
 
 namespace Universal_x86_Tuning_Utility
 {
@@ -76,12 +78,30 @@ namespace Universal_x86_Tuning_Utility
 
             Log.Logger = new LoggerConfiguration()
                 .WriteTo.Console(theme: AnsiConsoleTheme.Code, applyThemeToRedirectedOutput: true)
-                .WriteTo.File(LOGS_FOLDER + "uxtu_log.txt", 
-                    fileSizeLimitBytes: 8*1024*1024, // 8MB
+                .WriteTo.File(LOGS_FOLDER + "uxtu_log.txt",
+                    fileSizeLimitBytes: 8 * 1024 * 1024, // 8MB
                     rollingInterval: RollingInterval.Day
                 )
                 .CreateLogger();
-            
+
+            if (!IsVcRedistInstalled("x64") && !IsVcRedistInstalled("x86"))
+            {
+                var downloadVcRedist = MessageBox.Show("Microsoft Visual C++ Redistributable is not installed.\n" +
+                                                       "Would you like to download it now?",
+                    "Missing VC Redistributable",
+                    MessageBoxButton.YesNo,
+                    MessageBoxImage.Warning);
+                if (downloadVcRedist == MessageBoxResult.Yes)
+                {
+                    Process.Start(
+                        new ProcessStartInfo("https://learn.microsoft.com/en-us/cpp/windows/latest-supported-vc-redist")
+                            { UseShellExecute = true });
+                }
+
+                Shutdown();
+                return;
+            }
+
             try
             {
                 try
@@ -96,7 +116,10 @@ namespace Universal_x86_Tuning_Utility
 
                 _host = Host
                     .CreateDefaultBuilder()
-                    .ConfigureAppConfiguration(c => { c.SetBasePath(Path.GetDirectoryName(Assembly.GetEntryAssembly()!.Location)); })
+                    .ConfigureAppConfiguration(c =>
+                    {
+                        c.SetBasePath(Path.GetDirectoryName(Assembly.GetEntryAssembly()!.Location));
+                    })
                     .ConfigureLogging(logging =>
                     {
                         logging.ClearProviders();
@@ -115,10 +138,12 @@ namespace Universal_x86_Tuning_Utility
                             Settings.Default.isASUS = false;
                             Settings.Default.Save();
 
-                            if (product.Contains("ROG") || product.Contains("TUF") || product.Contains("Ally") || product.Contains("Flow") || product.ToLower().Contains("vivobook") || product.ToLower().Contains("zenbook"))
+                            if (product.Contains("ROG") || product.Contains("TUF") || product.Contains("Ally") ||
+                                product.Contains("Flow") || product.ToLower().Contains("vivobook") ||
+                                product.ToLower().Contains("zenbook"))
                             {
                                 wmi = new ASUSWmi();
-                            
+
                                 services.AddSingleton(wmi);
                                 services.AddSingleton<XgMobileConnectionService>();
 
@@ -200,13 +225,15 @@ namespace Universal_x86_Tuning_Utility
                 if (!createdNew)
                 {
                     _logger.LogWarning("Failed to start app, as there is already running uxtu instance. Shutting down");
-                    MessageBox.Show("An instance of Universal x86 Tuning Utility is already open!", "Error starting Universal x86 Tuning Utility");
+                    MessageBox.Show("An instance of Universal x86 Tuning Utility is already open!",
+                        "Error starting Universal x86 Tuning Utility");
                     // Close the new instance
                     Shutdown();
                     return;
                 }
 
-                if (File.Exists("C:\\Universal.x86.Tuning.Utility.V2.msi")) File.Delete("C:\\Universal.x86.Tuning.Utility.V2.msi");
+                if (File.Exists("C:\\Universal.x86.Tuning.Utility.V2.msi"))
+                    File.Delete("C:\\Universal.x86.Tuning.Utility.V2.msi");
 
                 Family.setCpuFamily();
 
@@ -217,7 +244,8 @@ namespace Universal_x86_Tuning_Utility
                 {
                     Settings.Default.Path = path;
                     Settings.Default.FirstBoot = false;
-                    if (Family.FAM > Family.RyzenFamily.Rembrandt || Family.FAM == Family.RyzenFamily.Mendocino) Settings.Default.polling = 3;
+                    if (Family.FAM > Family.RyzenFamily.Rembrandt || Family.FAM == Family.RyzenFamily.Mendocino)
+                        Settings.Default.polling = 3;
                     Settings.Default.Save();
 
                     //PowerPlans.SetPowerValue("scheme_current", "sub_processor", "PERFAUTONOMOUS", 1, true);
@@ -236,11 +264,17 @@ namespace Universal_x86_Tuning_Utility
                         _logger.LogError(ex, "Failed to unblock files in {dir} directory", path);
                     }
                 }
+
                 try
                 {
-                    if (IsInternetAvailable()) if (Settings.Default.UpdateCheck) CheckForUpdate();
+                    if (IsInternetAvailable())
+                        if (Settings.Default.UpdateCheck)
+                            CheckForUpdate();
                 }
-                catch (Exception ex) { _logger.LogError(ex, "Failed to check for updates"); }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Failed to check for updates");
+                }
 
                 await _host.StartAsync();
 
@@ -252,11 +286,11 @@ namespace Universal_x86_Tuning_Utility
                 {
                     _logger.LogError(ex, "Failed to sync game library");
                 }
-            } 
-            catch (Exception ex) 
-            { 
+            }
+            catch (Exception ex)
+            {
                 Log.Logger.Fatal(ex, "Failed to build and start a host");
-                MessageBox.Show(ex.Message); 
+                MessageBox.Show(ex.Message);
             }
         }
 
@@ -268,8 +302,12 @@ namespace Universal_x86_Tuning_Utility
 
             if (isUpdateAvailable)
             {
-                if (updateManager._newVersion.StartsWith("3.")) ToastNotification.ShowToastNotification("Phantom Control Centre Now Available!", $"Head to the Phantom Control Centre GitHub releases page to easily download the latest build!");
-                else ToastNotification.ShowToastNotification("New Update Available!", $"Head to the settings menu to easily download the new Universal x86 Tuning Utility update!");
+                if (updateManager._newVersion.StartsWith("3."))
+                    ToastNotification.ShowToastNotification("Phantom Control Centre Now Available!",
+                        $"Head to the Phantom Control Centre GitHub releases page to easily download the latest build!");
+                else
+                    ToastNotification.ShowToastNotification("New Update Available!",
+                        $"Head to the settings menu to easily download the new Universal x86 Tuning Utility update!");
             }
         }
 
@@ -289,17 +327,27 @@ namespace Universal_x86_Tuning_Utility
             }
         }
 
+        private bool IsVcRedistInstalled(string arch)
+        {
+            var baseKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+            using var key = baseKey.OpenSubKey(
+                $@"SOFTWARE\WOW6432Node\Microsoft\VisualStudio\14.0\VC\Runtimes\{arch}");
+            if (key == null) return false;
+
+            return key.GetValue("Installed") is int i && i == 1;
+        }
+
         /// <summary>
         /// Occurs when the application is closing.
         /// </summary>
         private async void OnExit(object sender, ExitEventArgs e)
         {
-           if (Family.TYPE != Family.ProcessorType.Intel) SMUCommands.RyzenAccess.Deinitialize();
+            if (Family.TYPE != Family.ProcessorType.Intel) SMUCommands.RyzenAccess.Deinitialize();
 
             await _host.StopAsync();
 
             _host.Dispose();
-            
+
             if (Games.CustomGameIconsDirectoryPath != null) //will be refactored in the future
             {
                 Directory.Delete(Games.CustomGameIconsDirectoryPath, true);
@@ -318,10 +366,7 @@ namespace Universal_x86_Tuning_Utility
         static void UnblockFilesInDirectory(string directoryPath)
         {
             // Unblock files in the current directory
-            Parallel.ForEach(Directory.GetFiles(directoryPath), filePath =>
-            {
-                UnblockFile(filePath);
-            });
+            Parallel.ForEach(Directory.GetFiles(directoryPath), filePath => { UnblockFile(filePath); });
         }
 
         static void UnblockFile(string filePath)
@@ -350,4 +395,3 @@ namespace Universal_x86_Tuning_Utility
         }
     }
 }
-
